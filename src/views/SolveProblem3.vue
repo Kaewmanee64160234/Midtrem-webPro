01e3150cd88014e13e77b293cfb10669475dd38d
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-01-16 17:27:19 +0700

    setting project

---
+++ src/views/SolveProblem3.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const products = ref(
+  Array.from(Array(100).keys()).map((item) => {
+    return {
+      id: item,
+      name: "Product " + (item + 1),
+      price: (Math.floor(Math.random() * 100) + 1) * 10,
+    };
+  })
+);
+</script>
+<template>
+  <div>
+    {{ products }}
+  </div>
+</template>
---
+++ src/views/SolveProblem4.vue
@@ -0,0 +1,4 @@
+<script setup lang="ts"></script>
+<template>
+  <div>ใส่ FirstComponent และ SecondComponent</div>
+</template>
